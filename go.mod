910bc79b48d8a4b26c8bd64fa4cfaca1b5f5be25
Edward <crazybber@outlook.com>
2020-01-09 18:51:46 +0800

    imp service

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/micro-community/x-edge
 go 1.13
 
 require (
+	github.com/go-log/log v0.1.0
 	github.com/golang/protobuf v1.3.2
 	github.com/google/uuid v1.1.1
 	github.com/micro-community/x-micro-edge v0.0.0-20200106050623-af33b580acb7
--- service.go
+++ service.go
@@ -4,10 +4,18 @@ import (
 	"sync"
 
 	"github.com/micro-community/x-edge/config"
+	ecli "github.com/micro-community/x-edge/node/client"
 	esrv "github.com/micro-community/x-edge/node/server"
 	"github.com/micro-community/x-edge/node/transport/tcp"
 	micro "github.com/micro/go-micro"
-	"github.com/micro/go-micro/server"
+	"github.com/micro/go-micro/client"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/micro/cli"
+	"github.com/micro/go-micro/util/log"
 )
 
 type service struct {
@@ -21,11 +29,8 @@ type service struct {
 
 func newService(opts ...Option) micro.Service {
 	options := newOptions(opts...)
-
-	svr := esrv.NewServer(server.WithRouter(esrv.DefaultRouter()))
-
 	nodeService := micro.NewService(
-		micro.Server(svr),
+		micro.Server(esrv.NewServer()),
 		micro.Name(config.XMicroEdgeServiceName),
 		micro.Address(config.XMicroEdgeServiceAddr),
 		micro.Transport(tcp.NewTransport()),
@@ -45,34 +50,193 @@ func newService(opts ...Option) micro.Service {
 	return s
 }
 
-// //RunProc Listen to The Device Edge Server (Gateway、Controller etc)
-// func RunProc() {
+func (s *service) start() error {
+	s.Lock()
+	defer s.Unlock()
 
-// 	r := esrv.DefaultRouter()
+	if s.running {
+		return nil
+	}
 
-// 	svr := esrv.NewServer(server.WithRouter(r))
+	l, err := s.listen("tcp", s.opts.Address)
+	if err != nil {
+		return err
+	}
 
-// 	t := ts.CreateTransport(config.XMicroEdgeServiceTransport)
-// 	// Create a new service. Optionally include some options here.
-// 	service := micro.NewService(
-// 		micro.Server(svr),
-// 		micro.Name(config.XMicroEdgeServiceName),
-// 		micro.Version("latest"),
-// 		micro.Address(config.XMicroEdgeServiceAddr),
-// 		micro.Transport(t),
-// 		micro.Metadata(map[string]string{
-// 			"type": "protocol-controller-server",
-// 		},
-// 		),
-// 	)
+	s.opts.Address = l.Addr().String()
 
-// 	service.Init()
+	for _, fn := range s.opts.BeforeStart {
+		if err := fn(); err != nil {
+			return err
+		}
+	}
 
-// 	//register server message router
-// 	router.RegisterProtocolHandler(svr, new(router.ProtocolServer))
+	for _, fn := range s.opts.AfterStart {
+		if err := fn(); err != nil {
+			return err
+		}
+	}
 
-// 	// Run the server
-// 	if err := service.Run(); err != nil {
-// 		fmt.Println(err)
-// 	}
-// }
+	s.exit = make(chan chan error, 1)
+	s.running = true
+
+	go func() {
+		ch := <-s.exit
+		ch <- l.Close()
+	}()
+
+	log.Logf("Listening on %v", l.Addr().String())
+	return nil
+}
+
+func (s *service) stop() error {
+	s.Lock()
+	defer s.Unlock()
+
+	if !s.running {
+		return nil
+	}
+
+	for _, fn := range s.opts.BeforeStop {
+		if err := fn(); err != nil {
+			return err
+		}
+	}
+
+	ch := make(chan error, 1)
+	s.exit <- ch
+	s.running = false
+
+	log.Log("Stopping")
+
+	for _, fn := range s.opts.AfterStop {
+		if err := fn(); err != nil {
+			if chErr := <-ch; chErr != nil {
+				return chErr
+			}
+			return err
+		}
+	}
+
+	return <-ch
+}
+
+func (s *service) Client() client.Client {
+	// rt := mhttp.NewRoundTripper(
+	// 	mhttp.WithRegistry(registry.DefaultRegistry),
+	// )
+	return ecli.NewClient()
+}
+
+func (s *service) Handle(pattern string, handler edge.Handler) {
+	var seen bool
+	for _, ep := range s.srv.Endpoints {
+		if ep.Name == pattern {
+			seen = true
+			break
+		}
+	}
+
+	// register the handler
+	//	s.mux.Handle(pattern, handler)
+}
+
+func (s *service) HandleFunc(pattern string, handler func(node.ResponseWriter, *node.Request)) {
+	var seen bool
+	// for _, ep := range s.srv.Endpoints {
+	// 	if ep.Name == pattern {
+	// 		seen = true
+	// 		break
+	// 	}
+	// }
+	// if !seen {
+	// 	s.srv.Endpoints = append(s.srv.Endpoints, &registry.Endpoint{
+	// 		Name: pattern,
+	// 	})
+	// }
+
+	s.mux.HandleFunc(pattern, handler)
+}
+
+func (s *service) Init(opts ...Option) error {
+	for _, o := range opts {
+		o(&s.opts)
+	}
+
+	serviceOpts := []micro.Option{}
+
+	if len(s.opts.Flags) > 0 {
+		serviceOpts = append(serviceOpts, micro.Flags(s.opts.Flags...))
+	}
+
+	serviceOpts = append(serviceOpts, micro.Action(func(ctx *cli.Context) {
+
+		if name := ctx.String("server_name"); len(name) > 0 {
+			s.opts.Name = name
+		}
+
+		if ver := ctx.String("server_version"); len(ver) > 0 {
+			s.opts.Version = ver
+		}
+
+		if id := ctx.String("server_id"); len(id) > 0 {
+			s.opts.ID = id
+		}
+
+		if addr := ctx.String("server_address"); len(addr) > 0 {
+			s.opts.Address = addr
+		}
+
+		if adv := ctx.String("server_advertise"); len(adv) > 0 {
+			s.opts.Advertise = adv
+		}
+
+		if s.opts.Action != nil {
+			s.opts.Action(ctx)
+		}
+	}))
+
+	s.opts.Service.Init(serviceOpts...)
+	//	srv := s.genSrv()
+	//	srv.Endpoints = s.srv.Endpoints
+	//	s.srv = srv
+
+	return nil
+}
+
+func (s *service) Run() error {
+	if err := s.start(); err != nil {
+		return err
+	}
+
+	// start reg loop
+	ex := make(chan bool)
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGTERM, syscall.SIGINT)
+
+	select {
+	// wait on kill signal
+	case sig := <-ch:
+		log.Logf("Received signal %s", sig)
+	// wait on context cancel
+	case <-s.opts.Context.Done():
+		log.Logf("Received context shutdown")
+	}
+
+	// exit reg loop
+	close(ex)
+
+	return s.stop()
+}
+
+// Options returns the options for the given service
+func (s *service) Options() Options {
+	return s.opts
+}
+
+func (s *service) listen(network, addr string) (net.Listener, error) {
+	var l net.Listener
+
+	return l, nil
+}
