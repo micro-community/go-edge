e38fd021a95f4d07cddc71f92bb55a43f3930c1b
Edward <crazybber@outlook.com>
2020-01-19 11:47:29 +0800

    update codes

--- node/transport/options.go
+++ node/transport/options.go
@@ -1,18 +1,16 @@
-package tcp
+package transport
 
 import (
 	"context"
 	"github.com/micro/go-micro/transport"
-
-	nts "github.com/micro-community/x-edge/node/transport"
 )
 
 // WithExtractor should be used to setup a extractor
-func WithExtractor(dex nts.DataExtractor) transport.Option {
+func WithExtractor(dex DataExtractor) transport.Option {
 	return func(o *transport.Options) {
 		if o.Context == nil {
 			o.Context = context.Background()
 		}
-		o.Context = context.WithValue(o.Context, nts.DataExtractorFuncKey{}, dex)
+		o.Context = context.WithValue(o.Context, DataExtractorFuncKey{}, dex)
 	}
 }
--- options.go
+++ options.go
@@ -1,15 +1,15 @@
 package edge
 
 import (
-	ns "github.com/micro-community/x-edge/node/server"
+	nts "github.com/micro-community/x-edge/node/transport"
 	service "github.com/micro/go-micro/service"
 )
 
 // Options  of edge node serivices
 
 //WithExtractor edge message
-func WithExtractor(de ns.DataExtractor) service.Option {
+func WithExtractor(de nts.DataExtractor) service.Option {
 	return func(o *service.Options) {
-		o.Server.Init(ns.Extractor(de))
+		o.Transport.Init(nts.WithExtractor(de))
 	}
 }
