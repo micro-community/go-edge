b3e7ca4040072a33e448544cd25baac4207bb214
danny-gao <56922102+danny-gao@users.noreply.github.com>
2020-04-03 23:44:41 +0800

    new opt.server first

--- edge/service.go
+++ edge/service.go
@@ -14,6 +14,9 @@ import (
 	"github.com/micro/go-micro/v2/logger"
 	"github.com/micro/go-micro/v2/server"
 	"github.com/micro/go-micro/v2/util/wrapper"
+
+	_ "github.com/micro-community/x-edge/node/client"
+	nserver "github.com/micro-community/x-edge/node/server"
 )
 
 type service struct {
@@ -29,7 +32,9 @@ func newService(opts ...Option) Service {
 	options := newOptions(opts...)
 	// service name
 	// options.Server is empty, so it is crash
-	// we should new options.Server  first  
+	// we should new options.Server  first
+	options.Server = nserver.NewServer()
+
 	serviceName := options.Server.Options().Name
 
 	// TODO: better accessors
